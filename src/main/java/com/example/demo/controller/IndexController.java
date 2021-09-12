cfbd123f1ae4cf34c419dabc320a696d2ded252d
hotan486 <hotan486@naver.com>
2021-09-12 16:02:46 +0900

    ㄴㅇㄹㄴㅇㄹ

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.config.auth.dto.SessionUser;
 import com.example.demo.domain.PostsRepository;
+import com.example.demo.domain.user.User;
 import com.example.demo.dto.HelloResponseDto;
 import com.example.demo.dto.PostsResponseDto;
 import com.example.demo.service.PostsService;
@@ -12,15 +14,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final PostsService postsService;
-
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user =
+                (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            System.out.println(user.getName());
+            String name = user.getName();
+            model.addAttribute("userName", name);
+        }
         return "index";
     }
 
@@ -36,5 +48,7 @@ public class IndexController {
 
         return "posts-update";
     }
+
+
 
 }
